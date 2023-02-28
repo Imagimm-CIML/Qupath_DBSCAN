c9bdd2dfd6f38f2ff8782b1e97e03af527ce604e
matfallet <41480459+matfallet@users.noreply.github.com>
2023-02-28 17:50:48 +0100

    Add files via upload

--- script_complet_DBSCAN.groovy
+++ script_complet_DBSCAN.groovy
@@ -1,6 +1,5 @@
 import org.apache.commons.math3.ml.clustering.DBSCANClusterer
 import org.apache.commons.math3.ml.clustering.DoublePoint
-
 import qupath.imagej.processing.RoiLabeling
 import qupath.imagej.tools.IJTools
 import qupath.lib.analysis.features.ObjectMeasurements
@@ -9,11 +8,9 @@ import qupath.lib.objects.PathObject
 import qupath.lib.objects.PathObjects
 import qupath.lib.regions.ImagePlane
 import qupath.opencv.ops.ImageOps
-
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
-
 import static qupath.lib.gui.scripting.QPEx.saveDetectionMeasurements
 import static qupath.lib.scripting.QP.*
 
@@ -29,10 +26,7 @@ double micronsBetweenCentroids = 100
 //Minimum number of objects needed to be considered a cluster
 int minPts = 3
 boolean baseClasses = false
-
 double eps = micronsBetweenCentroids/getCurrentServer().getPixelCalibration().getPixelWidthMicrons()
-print eps
-
 
 // detection
 for (annotation in annotations) {
@@ -43,99 +37,93 @@ for (annotation in annotations) {
 }
 
 
-
-//Get the classes you want to analyze. Avoids Negative and no class by default.
-Set classSet = []
-List classList = []
-if (!baseClasses){
-    
-    for (object in getDetectionObjects()) {
-        c = object.getPathClass()
-        if (c != getPathClass("Negative")){
-            classSet << c
-        }
-    }
-    
-    classList.addAll(classSet.findAll{
-        //If you only want one class, use it == getPathClass("MyClassHere") instead
-       it != null 
-    })
-    print classList
-}else{
-    for (object in getDetectionObjects()) {
-        parts = PathClassTools.splitNames(object.getPathClass())
-        parts.each{
-            if (it != "Negative"){
-                classSet << it
+// DBSCAN 
+for (annotation in annotations) {
+    hierarchy.getSelectionModel().clearSelection();
+    selectObjects { p -> p == annotation }
+    //Get the classes you want to analyze. Avoids Negative and no class by default.
+    Set classSet = []
+    List classList = []
+    if (!baseClasses){
+        for (object in getDetectionObjects()) {
+            c = object.getPathClass()
+            if (c != getPathClass("Negative")){
+                classSet << c
             }
         }
-    }
-    classList.addAll(classSet.findAll{
-        //If you only want one sub-class, use it == getPathClass("MyClassHere") instead
-       it != null
-    })
-    
-}
-
-classList.each{ c->
-    //Storage for stuff we do later. points will hold the XY coordinates as DoublePoint objects
-    List<DoublePoint> points = new ArrayList<DoublePoint>()
-    //The Map allows us to use the DoublePoint to match the list of coordinates output by DBScan to the QuPath object
-    Map< DoublePoint, double> pointMap = [:]
-    
-    //Get the objects of interest for this class or sub-class
-    if(baseClasses){
-        batch = getDetectionObjects().findAll{it.getPathClass().toString().contains(c)}
-        text = c
+        
+        classList.addAll(classSet.findAll{
+            //If you only want one class, use it == getPathClass("MyClassHere") instead
+           it != null 
+        })
+        print classList
     }else{
-        batch = getDetectionObjects().findAll{it.getPathClass() == c}
-        text = c.getName()
+        for (object in getDetectionObjects()) {
+            parts = PathClassTools.splitNames(object.getPathClass())
+            parts.each{
+                if (it != "Negative"){
+                    classSet << it
+                }
+            }
+        }
+        classList.addAll(classSet.findAll{
+            //If you only want one sub-class, use it == getPathClass("MyClassHere") instead
+           it != null
+        })
+        
     }
     
-    //print batch.size()
-    //Prep each object being analyzed for clustering.
-    batch.eachWithIndex{d,x->
-        //create the unique identifier, if you want to look back at details
-        //d.getMeasurementList().putMeasurement("ID",(double)x)
+    classList.each{ c->
+        //Storage for stuff we do later. points will hold the XY coordinates as DoublePoint objects
+        List<DoublePoint> points = new ArrayList<DoublePoint>()
+        //The Map allows us to use the DoublePoint to match the list of coordinates output by DBScan to the QuPath object
+        Map< DoublePoint, double> pointMap = [:]
         
-        //Reset previous cluster analyses for the given cell
-        d?.getMeasurementList().removeMeasurements("Cluster "+text)
+        //Get the objects of interest for this class or sub-class
+        if(baseClasses){
+            batch = getDetectionObjects().findAll{it.getPathClass().toString().contains(c)}
+            text = c
+        }else{
+            batch = getDetectionObjects().findAll{it.getPathClass() == c}
+            text = c.getName()
+        }
         
-        //create the linker between the ID and the centroid
-        double [] point = [d.getROI().getCentroidX(), d.getROI().getCentroidY()]
-        DoublePoint dpoint = new DoublePoint(point)
-        //print dpoint
-        points[x] = dpoint
-        //Key point here, each index (cell in most cases) is tracked and matched to it's XY coordinates
-        pointMap[dpoint]= (double)x
-    }
-    
-    //print points if you want to see them all
-    def showClosure = {detail ->
-        println "Cluster : " + detail.cluster + " Point : " + detail.point + " Label : "+ detail.labels
-        print "labels "+(int)detail.labels
-        print "cluster"+detail.cluster
+        //print batch.size()
+        //Prep each object being analyzed for clustering.
+        batch.eachWithIndex{d,x->
+            //create the unique identifier, if you want to look back at details
+            //d.getMeasurementList().putMeasurement("ID",(double)x)
+            
+            //Reset previous cluster analyses for the given cell
+            d?.getMeasurementList().removeMeasurements("Cluster "+text)
+            
+            //create the linker between the ID and the centroid
+            double [] point = [d.getROI().getCentroidX(), d.getROI().getCentroidY()]
+            DoublePoint dpoint = new DoublePoint(point)
+            //print dpoint
+            points[x] = dpoint
+            //Key point here, each index (cell in most cases) is tracked and matched to it's XY coordinates
+            pointMap[dpoint]= (double)x
+        }
         
-        //this uses the label (the index from the "batch") to access the correct cell, and apply a measurement with the correct cluster number
-        batch[detail.labels]?.getMeasurementList()?.putMeasurement("Cluster "+text,detail.cluster )
-        batch[detail.labels]?.getMeasurementList()?.putMeasurement("Cluster Size "+text,detail.clusterSize )
-    }
-    
-    //Main run statements
-    
-     def imageData = getCurrentImageData()
-     def hierarchy = imageData.getHierarchy()
-     def annotations = hierarchy.getAnnotationObjects()
-    
-    for (annotation in annotations) {
-        // DBSCAN
+        //print points if you want to see them all
+        def showClosure = {detail ->
+            println "Cluster : " + detail.cluster + " Point : " + detail.point + " Label : "+ detail.labels
+            print "labels "+(int)detail.labels
+            print "cluster"+detail.cluster
+            
+            //this uses the label (the index from the "batch") to access the correct cell, and apply a measurement with the correct cluster number
+            batch[detail.labels]?.getMeasurementList()?.putMeasurement("Cluster "+text,detail.cluster )
+            batch[detail.labels]?.getMeasurementList()?.putMeasurement("Cluster Size "+text,detail.clusterSize )
+        }
+     }
+     
+        //Main run statements
         DBSCANClusterer DBScan = new DBSCANClusterer(eps, minPts)
         collectDetails(DBScan.cluster(points), pointMap).each(showClosure)
         // classify
         runObjectClassifier("cluster");
-        }
-        //fireHierarchyUpdate()
-    
-}
+            
+}// loop all regions
 
 print "DONE"
